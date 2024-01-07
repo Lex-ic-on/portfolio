57b2596c10821f7e6b1cfc40e38f700160588424
Lex-ic-on <summarit2.5@gmail.com>
2024-01-07 14:06:09 +0000

    ready to make footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import Hero from '../components/hero-tier1'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-       <h1 className={styles.pageTitle}>Lex</h1>
+      <Hero
+        dir="1exicon.com"
+        title="Lex"
+        statsOn
+      />
+      <ul className={styles.portalLink}>
+        <li>
+          <Link href='./Works'><h3 className={styles.bold}>See my works &gt;&gt;&gt;</h3></Link>
+        </li>
+        <li>
+          <Link href='mailto:contact.4194304@gmail.com'><h3 className={styles.bold}>Contact me &gt;&gt;&gt;</h3></Link>
+        </li>
+      </ul>
     </main>
   )
 }
--- src/components/hero-tier1/index.tsx
+++ src/components/hero-tier1/index.tsx
@@ -1,4 +1,11 @@
 import styles from './hero-t1.module.css'
 
-export default function T1() {
+export default function T1({ dir, title, statsOn=false }) {
+	return (
+		<div className={styles.hero}>
+			<h5 className={styles.dir}>{dir}</h5>
+			<h1 className={styles.pageTitle}>{title}</h1>
+			{statsOn && <p>Designer, Developer</p>}
+		</div>
+		)
 }
